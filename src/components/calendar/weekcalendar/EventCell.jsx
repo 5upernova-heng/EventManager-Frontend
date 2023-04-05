c39c2a4f2b2ba5b4b0a065e220de95bd28243399
Hengyu <h3ngyu@gmail.com>
2023-04-06 01:15:20 +0800

    Update: Delete hard code data in event

--- src/components/calendar/weekcalendar/EventCell.jsx
+++ src/components/calendar/weekcalendar/EventCell.jsx
@@ -1,25 +1,31 @@
 import { Modal } from "bootstrap/js/dist/modal";
 
-function calStyle(event) {
-    return {
-        top: `${(event.startTime / 24) * 100}%`,
-        bottom: `${(1 - event.endTime / 24) * 100}%`,
-    };
-}
-
 const EventCell = ({ event, handleClick }) => {
+    const calStyle = () => {
+        return {
+            top: `${(event.startTime / 24) * 100}%`,
+            bottom: `${(1 - event.endTime / 24) * 100}%`,
+        };
+    };
+
+    const renderStyleClass = () => {
+        const baseStyle =
+            "position-absolute p-2 start-0 end-0 ms-1 me-2 border rounded overflow-auto ";
+        const backgroundColorSet = ["bg-primary", "bg-success", "bg-info"];
+        return baseStyle + backgroundColorSet[event.category];
+    };
     return (
         <div
-            className="position-absolute p-2 start-0 end-0 ms-1 me-2 border bg-primary rounded overflow-auto"
+            className={renderStyleClass()}
             style={calStyle(event)}
             type="button"
             data-bs-toggle="modal"
             data-bs-target="#modifyEvent"
             onClick={() => handleClick(event)}
         >
-            <p className="fs-5 mb-0 text-white fw-bold">课程</p>
+            <p className="fs-5 mb-0 text-white fw-bold">{`${event.title}`}</p>
             <p className="fs-6 mb-0 text-white">{`${event.startTime}:00-${event.endTime}:00`}</p>
-            <p className="fs-6 mb-0 text-white">这是一个课程描述</p>
+            <p className="fs-6 mb-0 text-white">{`${event.description}`}</p>
         </div>
     );
 };
--- src/components/calendar/weekcalendar/WeekCalendar.jsx
+++ src/components/calendar/weekcalendar/WeekCalendar.jsx
@@ -5,8 +5,30 @@ import CalendarBody from "./CalendarBody";
 class WeekCalendar extends Component {
     state = {
         events: [
-            { startTime: 8, endTime: 10, day: 0 },
-            { startTime: 15, endTime: 20, day: 2 },
+            {
+                startTime: 8,
+                endTime: 10,
+                day: 0,
+                category: 0,
+                title: "课程事件",
+                description: "这是一个课程事件",
+            },
+            {
+                startTime: 15,
+                endTime: 20,
+                day: 2,
+                category: 1,
+                title: "个人事件",
+                description: "这是一个学生个人事件",
+            },
+            {
+                startTime: 7,
+                endTime: 12,
+                day: 4,
+                category: 2,
+                title: "团体事件",
+                description: "这是一个学生发起的团体事件",
+            },
         ],
     };
     render() {
