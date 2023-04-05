a25b9c628aa81cc9c940565b6daecca903d8092e
Hengyu <h3ngyu@gmail.com>
2023-04-05 23:59:55 +0800

    Extract events to WeekCalendar component

--- src/components/calendar/weekcalendar/CalendarBody.jsx
+++ src/components/calendar/weekcalendar/CalendarBody.jsx
@@ -1,58 +1,55 @@
 import CalendarTimeColumn from "./CalendarTimeColumn";
 import CalendarDateColumn from "./CalendarDateColumn";
+import React, { Component } from "react";
 import { getWeekDates, dateDiff } from "../../../utils/calDate";
-import { useState } from "react";
 
-// TODO: hardcoded
-
-const CalendarBody = ({ date }) => {
-    const [distEvents, setDistEvents] = useState([
-        [{ startTime: 8, endTime: 10, date: new Date(2023, 2, 26) }],
-        [],
-        [{ startTime: 15, endTime: 20, date: new Date(2023, 2, 28) }],
-        [],
-        [],
-        [],
-        [],
-    ]);
-    const distribute = (dates, events) => {
-        // distribute events at this week to an array
-        // while each array is an array of event
-        const startDate = new Date(dates[0]);
-        const endDate = new Date(dates[6]);
-        startDate.setHours(0, 0, 0, 0);
-        endDate.setHours(0, 0, 0, 0);
-        const dEvents = dates.map(() => []);
-        events.map((e) => {
-            if (startDate <= e.date && endDate <= e.date) {
-                console.log(e.date);
-                const index = dateDiff(e.date, startDate);
-                dEvents[index].push(e);
-            }
+class CalendarBody extends Component {
+    constructor(props) {
+        super(props);
+        const { events } = this.props;
+        const newDistrube = [[], [], [], [], [], [], []];
+        events.map((event) => {
+            newDistrube[event.day].push(event);
         });
-        setDistEvents(dEvents);
-    };
-    const createColumn = (date) => {
-        const dates = getWeekDates(date);
-        return dates.map((d, index) => (
-            <div className="col p-0" key={index}>
-                <CalendarDateColumn events={distEvents[index]} />
-            </div>
-        ));
-    };
-    return (
-        <>
-            <div
-                style={{ maxHeight: "450px" }}
-                className="row overflow-auto border"
-            >
-                <div className="col-1 p-0">
-                    <CalendarTimeColumn />
-                </div>
-                {createColumn(date)}
-            </div>
-        </>
-    );
-};
+        this.state = {
+            distributedEvents: newDistrube,
+        };
+    }
 
+    render() {
+        const { date, events } = this.props;
+        const distribute = () => {
+            // distribute events at this week to an array
+            // while each array is an array of event
+            const newDistrube = [[], [], [], [], [], [], []];
+            events.map((event) => {
+                newDistrube[event.day].push(event);
+            });
+            this.setState({ distributedEvents: newDistrube });
+        };
+        const createColumn = () => {
+            const dates = getWeekDates(date);
+            return dates.map((d, index) => (
+                <div className="col p-0" key={index}>
+                    <CalendarDateColumn
+                        events={this.state.distributedEvents[index]}
+                    />
+                </div>
+            ));
+        };
+        return (
+            <>
+                <div
+                    style={{ maxHeight: "450px" }}
+                    className="row overflow-auto border"
+                >
+                    <div className="col-1 p-0">
+                        <CalendarTimeColumn />
+                    </div>
+                    {createColumn()}
+                </div>
+            </>
+        );
+    }
+}
 export default CalendarBody;
--- src/components/calendar/weekcalendar/WeekCalendar.jsx
+++ src/components/calendar/weekcalendar/WeekCalendar.jsx
@@ -1,13 +1,24 @@
+import React, { Component } from "react";
 import CalendarHead from "./CalendarHead";
 import CalendarBody from "./CalendarBody";
 
-const WeekCalendar = ({ date }) => {
-    return (
-        <>
-            <CalendarHead date={date} />
-            <CalendarBody date={date} />
-        </>
-    );
-};
+class WeekCalendar extends Component {
+    state = {
+        events: [
+            { startTime: 8, endTime: 10, day: 0 },
+            { startTime: 15, endTime: 20, day: 2 },
+        ],
+    };
+    render() {
+        const { date } = this.props;
+        const { events } = this.state;
+        return (
+            <>
+                <CalendarHead date={date} />
+                <CalendarBody date={date} events={events} />
+            </>
+        );
+    }
+}
 
 export default WeekCalendar;
