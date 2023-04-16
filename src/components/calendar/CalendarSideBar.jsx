0cd8e411c38da14cfdf4296cb79b77408c57bf24
Hengyu <h3ngyu@gmail.com>
2023-04-16 15:50:04 +0800

    Fix: warnings

--- src/components/calendar/CalendarSideBar.jsx
+++ src/components/calendar/CalendarSideBar.jsx
@@ -10,7 +10,7 @@ const CalendarSideBar = ({ date, sync, toggleSync, setTimeInterval }) => {
                 <h4 className="m-0 p-1 text-center fw-bold">时间设置</h4>
                 <div className="row d-flex flex-column justify-content-between align-items-center">
                     <div className="col d-flex justify-content-between align-items-center my-2">
-                        <label for="syncRealTime">与自然时间同步</label>
+                        <label htmlFor="syncRealTime">与自然时间同步</label>
                         <input
                             type="checkbox"
                             id="syncRealTime"
@@ -23,7 +23,7 @@ const CalendarSideBar = ({ date, sync, toggleSync, setTimeInterval }) => {
                         <select
                             className="form-select-sm"
                             aria-label="Default select example"
-                            disabled={sync}
+                            defaultValue="0"
                             onChange={(e) => {
                                 setTimeInterval(
                                     intervalList[
@@ -32,9 +32,7 @@ const CalendarSideBar = ({ date, sync, toggleSync, setTimeInterval }) => {
                                 );
                             }}
                         >
-                            <option value="0" selected>
-                                1 秒/秒
-                            </option>
+                            <option value="0">1 秒/秒</option>
                             <option value="1">1 分钟/秒</option>
                             <option value="2">1 小时/秒</option>
                             <option value="3">1 天/秒</option>
