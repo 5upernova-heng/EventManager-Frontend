2a3391719049a826da7db8fde052c69637727b08
Hengyu <h3ngyu@gmail.com>
2023-04-26 00:40:00 +0800

    Fix: typo

--- src/components/time/Time.jsx
+++ src/components/time/Time.jsx
@@ -1,13 +1,6 @@
 import TimeString from "./TimeString";
 import TimeButton from "./TimeButton";
 
-function getDateString(date) {
-    return `
-    ${date.getFullYear()}/${date.getMonth() + 1}/${date.getDate()}
-    ${String(date.getHours()).padStart(2, "0")}:${String(
-        date.getMinutes()
-    ).padStart(2, "0")}:${String(date.getSeconds()).padStart(2, "0")}`;
-}
 const Time = ({ date, clickHandler, tick, sync }) => {
     return (
         <div className="d-flex align-items-center">
