c6c71c91562e98391f6adcb735ca781087dff729
Hengyu <h3ngyu@gmail.com>
2023-04-26 00:40:00 +0800

    Fix: typo

--- src/components/calendar/CalendarBar.jsx
+++ src/components/calendar/CalendarBar.jsx
@@ -1,13 +1,5 @@
 import Time from "../time/Time";
 
-function getDateString(date) {
-    return `
-    ${date.getFullYear()}/${date.getMonth() + 1}/${date.getDate()}
-    ${String(date.getHours()).padStart(2, "0")}:${String(
-        date.getMinutes()
-    ).padStart(2, "0")}:${String(date.getSeconds()).padStart(2, "0")}`;
-}
-
 function CalendarBar({ date, tick, sync, clickHandler }) {
     return (
         <>
