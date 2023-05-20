455ed0bb2180094307f488846dfb3f8d483258d7
Hengyu <h3ngyu@gmail.com>
2023-05-20 21:02:17 +0800

    Update: show tool tip after selected

--- src/components/map/MapNode.jsx
+++ src/components/map/MapNode.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import PropTypes from "prop-types";
 import "/src/styles/MapNode.css";
 import { MapContext } from "../../context/MapContextProvider";
@@ -8,23 +8,56 @@ function MapNode({ node, selected }) {
     const { fixedX, fixedY, scale, addNavPoint } = useContext(MapContext);
     const { id, x, y, name } = node;
 
+    const [hover, setHover] = useState(false);
+
+    const calStyle = () => {
+        return {
+            top: `${fixedY(y)}px`,
+            left: `${fixedX(x)}px`,
+            position: "absolute",
+            transform: `scale(${1 / scale})`,
+        };
+    };
+
+    const renderToolTip = () => {
+        return (
+            <div
+                className="position-absolute p-2 rounded"
+                style={{
+                    position: "absolute",
+                    top: "-.25rem",
+                    left: "50%",
+                    transform: `translateX(-50%) translateY(-100%) scale(${
+                        selected || hover ? 1 : 0
+                    })`,
+                    transition: "150ms",
+                    transformOrigin: "bottom",
+                    color: "white",
+                    background: "#333",
+                    textAlign: "center",
+                    width: "max-content",
+                }}
+            >
+                {name}
+            </div>
+        );
+    };
+
     return (
         <div
-            className={`p-2 rounded-circle map-node shadow ${
+            className={`p-2 rounded-circle shadow ${
                 selected ? "bg-primary" : "bg-success"
             }`}
-            style={{
-                top: `${fixedY(y)}px`,
-                left: `${fixedX(x)}px`,
-                position: "absolute",
-                transform: `scale(${1 / scale})`,
-            }}
+            style={calStyle()}
             onClick={() => {
                 addNavPoint(id);
             }}
+            onMouseEnter={() => setHover(true)}
+            onMouseLeave={() => setHover(false)}
             data-tooltip={`${id}-${name}`}
-            data-show={selected}
-        ></div>
+        >
+            {renderToolTip()}
+        </div>
     );
 }
 
