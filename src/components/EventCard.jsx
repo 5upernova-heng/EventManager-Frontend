86e1e6c597af7866ea5974aa73a8ceacc36a29e3
Hengyu <h3ngyu@gmail.com>
2023-05-25 17:54:05 +0800

    Fix: event card location

--- src/components/EventCard.jsx
+++ src/components/EventCard.jsx
@@ -1,10 +1,12 @@
-import React from "react";
+import React, { useContext } from "react";
 import PropTypes from "prop-types";
 import { getTimeString } from "../utils/calDate";
 import STYLE from "../style";
+import { MapContext } from "../context/MapContextProvider";
 
 function EventCard({ event }) {
-    const { startTime, endTime, title, doLoop, category } = event;
+    const { startTime, endTime, title, doLoop, category, location } = event;
+    const { getLocationName } = useContext(MapContext);
 
     const parseTimeString = (timeStamp) => {
         const date = new Date(timeStamp);
@@ -15,23 +17,26 @@ function EventCard({ event }) {
     const endTimeStr = parseTimeString(endTime);
 
     return (
-        <div className="d-flex my-3 p-2">
-            <div className="d-flex flex-column justify-content-between pe-1">
-                <p className="mb-1 fs-4 text-secondary">{startTimeStr}</p>
-                <p className="mb-0 fs-4 text-secondary">{endTimeStr}</p>
+        <div className="d-flex justify-content-between my-3 p-2">
+            <div className="d-flex">
+                <div className="d-flex flex-column justify-content-between pe-1">
+                    <p className="mb-1 fs-4 text-secondary">{startTimeStr}</p>
+                    <p className="mb-0 fs-4 text-secondary">{endTimeStr}</p>
+                </div>
+                <div
+                    className={`ms-3 me-4 p-1 bg-${STYLE.getCategoryColor(
+                        category
+                    )} rounded`}
+                ></div>
+                <div className="d-flex flex-column justify-content-between pe-1">
+                    <p className="fw-bold fs-3 mb-1">{title}</p>
+                    <p className="fs-4 mb-0">{`${STYLE.getCategoryLabel(
+                        category
+                    )} | ${STYLE.timeLabel[doLoop]} | ${getLocationName(
+                        location
+                    )}`}</p>
+                </div>
             </div>
-            <div
-                className={`ms-3 me-4 p-1 bg-${STYLE.getCategoryColor(
-                    category
-                )} rounded`}
-            ></div>
-            <div className="d-flex flex-column justify-content-between pe-1">
-                <p className="fw-bold fs-3 mb-1">{title}</p>
-                <p className="fs-4 mb-0">{`${STYLE.getCategoryLabel(
-                    category
-                )} | ${STYLE.timeLabel[doLoop]} | 教三楼 3-217`}</p>
-            </div>
-            <div style={{ width: "20rem" }}></div>
             <div className="d-flex align-items-center">
                 <p className="fs-4">3 小时后</p>
             </div>
