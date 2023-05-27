274ee78d6668a5fe1f8c8d73eae0eefd23e96f5f
Hengyu <h3ngyu@gmail.com>
2023-05-27 11:56:49 +0800

    Update: location card hover effect

--- src/components/map/LocationCard.jsx
+++ src/components/map/LocationCard.jsx
@@ -1,21 +1,24 @@
-import React, { useContext } from "react";
+import React, { useContext, useState } from "react";
 import PropTypes from "prop-types";
 import { MapContext } from "../../context/MapContextProvider";
 
 export default function LocationCard({ id, selected, toggleSelected }) {
     const { mode, navPoints, getLocationName, deleteNavPoint } =
         useContext(MapContext);
+    const [hover, setHover] = useState(false);
     return (
         <>
             <div
                 className="location-card d-flex align-items-center p-3 border border-3 border-secondary-subtle rounded rounded-4"
+                onMouseEnter={() => setHover(true)}
+                onMouseLeave={() => setHover(false)}
                 onClick={() => {
                     toggleSelected();
                 }}
             >
                 <div className="flex-grow-1">
                     <p className="mb-0 fs-4 fw-bold text-center">
-                        {selected && (
+                        {(hover || selected) && (
                             <i
                                 className="fa fa-arrow-right"
                                 aria-hidden="true"
@@ -24,7 +27,7 @@ export default function LocationCard({ id, selected, toggleSelected }) {
                         {getLocationName(id)}
                     </p>
                 </div>
-                {selected && navPoints.length > 2 && mode === 1 && (
+                {(hover || selected) && navPoints.length > 2 && mode === 1 && (
                     <button
                         onClick={() => {
                             deleteNavPoint(id);
