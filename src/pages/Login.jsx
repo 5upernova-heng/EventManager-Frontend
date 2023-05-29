ed15c4b44e3b3948e31caab0ed85ef365af088a6
Hengyu <h3ngyu@gmail.com>
2023-05-30 01:17:40 +0800

    Fix: login is unscrollable

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -4,14 +4,7 @@ import LoginFormGroup from "../components/forms/LoginFormGroup";
 
 function Login() {
     return (
-        <div
-            width="100%"
-            style={{
-                height: "100vh",
-                background:
-                    "radial-gradient(61.04% 89.69% at 100% 100%, rgba(200, 250, 255, 0.08) 0%, rgba(28, 210, 229, 0.08) 40.63%, rgba(28, 210, 229, 0) 100%), radial-gradient(43.78% 64.34% at 60.31% 100%, rgba(23, 74, 228, 0.08) 0%, rgba(23, 74, 228, 0) 100%), linear-gradient(180deg, rgba(23, 74, 228, 0) 29.44%, rgba(23, 74, 228, 0.06) 100%), linear-gradient(90deg, #F3F3F7 0%, #EBF0F9 100%)",
-            }}
-        >
+        <div>
             <div className="card rounded-4 shadow-lg bg-light position-absolute top-50 start-50 translate-middle p-5">
                 <img
                     src="/src/assets/bupt_logo.svg"
